5271cd8265783a0ae84e6c6e30c0b0f372f1ed01
Dinara Gainulina <dignara@yandex.ru>
2022-09-29 14:24:27 +0300

    feat: add basic new item form

--- src/components/AddNewItem.tsx
+++ src/components/AddNewItem.tsx
@@ -1,11 +1,34 @@
 import { useState } from 'react';
 import { AddItemButton } from '../styles';
+import { NewItemForm } from './NewItemForm';
 
 type AddNemItemProps = {
   buttonTitle: string;
   newCard: boolean;
+  onAdd(text: string): void;
 };
 
-export const AddNewItem = ({ buttonTitle, newCard }: AddNemItemProps) => {
-  return <AddItemButton newCard={newCard}>{buttonTitle}</AddItemButton>;
+export const AddNewItem = ({
+  buttonTitle,
+  newCard,
+  onAdd,
+}: AddNemItemProps) => {
+  const [showAddForm, setShowAddForm] = useState(false);
+
+  if (showAddForm) {
+    return (
+      <NewItemForm
+        onAdd={(text) => {
+          onAdd(text);
+          setShowAddForm(false);
+        }}
+      />
+    );
+  }
+
+  return (
+    <AddItemButton newCard={newCard} onClick={() => setShowAddForm(true)}>
+      {buttonTitle}
+    </AddItemButton>
+  );
 };
