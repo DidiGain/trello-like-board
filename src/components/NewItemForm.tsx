db0505e91ddb3933d1094006e339bf93f02cfb66
Dinara Gainulina <dignara@yandex.ru>
2022-09-29 17:15:53 +0300

    feat: add ref for autofocus from custom hook

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { NewItemButton, NewItemFormContainer, NewItemInput } from '../styles';
+import { useFocus } from '../utils/useFocus';
 
 type NewItemFormProps = {
   onAdd(text: string): void;
@@ -7,10 +8,20 @@ type NewItemFormProps = {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState('');
+  const inputRef = useFocus();
+
+  const handleAddedText = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') onAdd(text);
+  };
 
   return (
     <NewItemFormContainer>
-      <NewItemInput value={text} onChange={(e) => setText(e.target.value)} />
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onKeyDown={handleAddedText}
+      />
       <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
   );
